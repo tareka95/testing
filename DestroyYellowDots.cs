20f9ca178bddd22536623404706feb8e66cf753f
Tarek Al Ashi <tarekashi@Tareks-MacBook-Pro.local>
2017-03-10 17:45:09 +0200

    i did copy the file

---
+++ DestroyYellowDots.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyYellowDots : MonoBehaviour {
+	[HideInInspector]
+	public static bool YellowDestroy;
+	public AudioSource audioController;
+	public AudioClip playClip;
+	public ScoreManaging scoreManagerScript;
+
+	void Start ()
+	{
+		 audioController = GameObject.Find ("SoundEffect").GetComponent <AudioSource> ();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("Pacman")) {
+			scoreManagerScript.AddScore ();
+		//	GetComponent <AudioSource> ().PlayOneShot (playClip);
+			audioController.Play(); 
+			YellowDestroy = true;
+			Destroy (this.gameObject);
+		} 
+		else 
+		{
+			scoreManagerScript.once = true;
+			YellowDestroy = false;
+		}
+
+	}
+
+}
